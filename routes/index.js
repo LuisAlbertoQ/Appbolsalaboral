b507ba826144b6a69883e67149e15c54df518fd4
LuisAlbertoQ <quillaluis501@gmail.com>
2023-06-30 10:20:29 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,26 @@ var dbConn  = require('../lib/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  dbConn.query('SELECT * FROM egresados',function(err,rows){
+    if(err) {
+        req.flash('error', err);
+        res.render('index',{data:''});   
+    }else {
+        res.render('index',{data:rows});
+    }
+  });
+});
+
+router.post('/search', function(req, res, next) {
+  let name=req.body.search;
+  dbConn.query("SELECT * FROM egresados WHERE nombre LIKE ?", ['%' + name + '%'], function(err, rows) {
+    if(err) {
+        req.flash('error', err);
+        res.render('index',{data:''});   
+    }else {
+        res.render('index',{data:rows});
+    }
+  });
 });
 
 router.get('/admin/login', function(req, res, next) {
